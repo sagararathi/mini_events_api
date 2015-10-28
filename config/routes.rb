8d99f2f7456b56e1f50a6283645145f981fea3a8
sagararathi <bravo.sagar@gmail.com>
2015-10-28 12:12:26 -0400

    created GET route for api to get all events

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
 Rails.application.routes.draw do
+  scope module: :api, defaults: { format: 'json' } do
+    namespace :v1 do
+      namespace :events do
+        resources :nearests, only: [:index]
+      end
+    end
+  end
 end
